0efc75270c6686e6bfa09466966861b75419ad66
honolulu0 <1037494014@qq.com>
2026-04-18 11:05:04 +0800

    chore: ignore node_modules and backend dist

--- backend/dist/services/history/service.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.createHistoryService = createHistoryService;
-function createHistoryService(store) {
-    return {
-        list: () => store.listHistory(),
-        record: (input) => store.recordHistory(input)
-    };
-}
